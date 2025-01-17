4885527fc68a628c6254890b0811e64dcd8d0d8b
Danny Zhou <danny.zhou49@gmail.com>
2025-01-17 01:51:03 -0500

    implement qstash workflows for user onboarding

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,7 +1,35 @@
 import { Client as WorkFlowClient } from "@upstash/workflow";
+import { Client as QStashClient, resend } from "@upstash/qstash";
 import config from "@/lib/config";
 
 export const workflowClient = new WorkFlowClient({
   baseUrl: config.env.upstash.qstashUrl,
   token: config.env.upstash.qstashToken,
 });
+
+const qStashClient = new QStashClient({
+  token: config.env.upstash.qstashToken,
+});
+
+export const sendEmail = async ({
+  email,
+  subject,
+  message,
+}: {
+  email: string;
+  subject: string;
+  message: string;
+}) => {
+  await qStashClient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resendToken }),
+    },
+    body: {
+      from: "Danny Zhou <danny.zhou49@gmail.com>",
+      to: [email],
+      subject,
+      html: message,
+    },
+  });
+};
